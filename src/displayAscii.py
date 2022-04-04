4168cf7d3e15976fd6bebf9feb419cbe8092499b
adeliaaaa <monicadelia88@gmail.com>
2022-04-04 12:20:41 +0700

    New File Struct

---
+++ src/displayAscii.py
@@ -0,0 +1,50 @@
+def asciiGameName():
+    print("____  _______   _______  __   __  _______  _______  ___      _______ ")
+    print("|    ||       | |       ||  | |  ||       ||       ||   |    |       |")
+    print("|   ||   ____| |    _  ||  | |  ||____   ||____   ||   |    |    ___|")
+    print("|   ||  |____  |   |_| ||  |_|  | ____|  | ____|  ||   |    |   |___ ")
+    print("|   ||_____  | |    ___||       || ______|| ______||   |___ |    ___|")
+    print("|   | _____| | |   |    |       || |_____ | |_____ |       ||   |___ ")
+    print("|___||_______| |___|    |_______||_______||_______||_______||_______|")
+    print("Welcome to 15 puzzle!!!")
+    print()
+
+def asciiInsertFileName():
+    print(" ___                  _     ___ _ _                         ")
+    print("|_ _|_ _  ___ ___ _ _| |_  | __(_) |___ _ _  __ _ _ __  ___ ")
+    print("|  || ' \(_-</ -_) '_|  _| | _|| | / -_) ' \/ _` | '  \/ -_)")
+    print("|___|_||_/__/\___|_|  \__| |_| |_|_\___|_||_\__,_|_|_|_\___|")
+
+def asciiSolvingPuzzle():
+    print(" ___      _     _                 ")
+    print("/ __| ___| |_ _(_)_ _  __ _       ")
+    print("\__ \/ _ \ \ V / | ' \/ _` |_ _ _ ")
+    print("|___/\___/_|\_/|_|_||_\__, (_|_|_)")
+    print("                    |___/       ")
+    print()
+
+def asciiFound():
+    print("___                 _ _ ")
+    print("| __|__ _  _ _ _  __| | |")
+    print("| _/ _ \ || | ' \/ _` |_|")
+    print("|_|\___/\_,_|_||_\__,_(_)")
+    print("Persoalan dapat diselesaikan")
+    print()
+
+def asciiNotFound():
+    print(" _  _     _     ___                 _ _ ")
+    print("| \| |___| |_  | __|__ _  _ _ _  __| | |")
+    print("| .` / _ \  _| | _/ _ \ || | ' \/ _` |_|")
+    print("|_|\_\___/\__| |_|\___/\_,_|_||_\__,_(_)")
+    print("Persoalan tidak bisa diselesaikan")
+    print()
+
+def asciiFailedToSolve():
+    print("   ___                  ")
+    print("  / _ \ _ __ _ __ ______")
+    print(" | (_) | '_ \ '_ (_-<_-<")
+    print("  \___/| .__/ .__/__/__/")
+    print("       |_|  |_|         ")
+    print("You've waited more than 30 minutes!")
+    print("Try again or Try another puzzle")
+    print()
--- src/helperFunction.py
+++ src/helperFunction.py
@@ -1,6 +1,8 @@
 import random
 from numpy import *
 
+from displayAscii import *
+
 def getIndex(i,j):
     return ((4 * i) + j)
 
@@ -35,54 +37,6 @@ def createRandomPuzzle():
 
     return a
 
-def moveUp(matrix, i, j):
-    newMatrix = [[0,0,0,0], [0,0,0,0], [0,0,0,0], [0,0,0,0]]
-    for k in range (4):
-        for l in range (4):
-            newMatrix[k][l] = matrix[k][l]
-
-    temp = newMatrix[i-1][j]
-    newMatrix[i-1][j] = newMatrix[i][j]
-    newMatrix[i][j] = temp
-
-    return newMatrix
-
-def moveDown(matrix, i, j):
-    newMatrix = [[0,0,0,0], [0,0,0,0], [0,0,0,0], [0,0,0,0]]
-    for k in range (4):
-        for l in range (4):
-            newMatrix[k][l] = matrix[k][l]
-
-    temp = newMatrix[i+1][j]
-    newMatrix[i+1][j] = newMatrix[i][j]
-    newMatrix[i][j] = temp
-
-    return newMatrix
-
-def moveRight(matrix, i, j):
-    newMatrix = [[0,0,0,0], [0,0,0,0], [0,0,0,0], [0,0,0,0]]
-    for k in range (4):
-        for l in range (4):
-            newMatrix[k][l] = matrix[k][l]
-    
-    temp = newMatrix[i][j+1]
-    newMatrix[i][j+1] = newMatrix[i][j]
-    newMatrix[i][j] = temp
-
-    return newMatrix
-
-def moveLeft(matrix, i, j):
-    newMatrix = [[0,0,0,0], [0,0,0,0], [0,0,0,0], [0,0,0,0]]
-    for k in range (4):
-        for l in range (4):
-            newMatrix[k][l] = matrix[k][l]
-
-    temp = newMatrix[i][j-1]
-    newMatrix[i][j-1] = newMatrix[i][j]
-    newMatrix[i][j] = temp
-
-    return newMatrix
-
 def sumOfNotMatch(matrix):
     newMatrix = [[1,2,3,4], [5,6,7,8], [9,10,11,12], [13,14,15,16]]
 
@@ -93,10 +47,6 @@ def sumOfNotMatch(matrix):
                 if(matrix[i][j] != newMatrix[i][j]):
                     sum = sum + 1
     return sum
-
-def realDepth(depth):
-    a = depth
-    return a * (-1)
 
 def insertFile():
     asciiInsertFileName()
@@ -162,54 +112,3 @@ def printUrutanMatrix(path):
             printMatrix(matrix)
             print()
         stepCounter = stepCounter + 1
-
-def asciiGameName():
-    print("____  _______   _______  __   __  _______  _______  ___      _______ ")
-    print("|    ||       | |       ||  | |  ||       ||       ||   |    |       |")
-    print("|   ||   ____| |    _  ||  | |  ||____   ||____   ||   |    |    ___|")
-    print("|   ||  |____  |   |_| ||  |_|  | ____|  | ____|  ||   |    |   |___ ")
-    print("|   ||_____  | |    ___||       || ______|| ______||   |___ |    ___|")
-    print("|   | _____| | |   |    |       || |_____ | |_____ |       ||   |___ ")
-    print("|___||_______| |___|    |_______||_______||_______||_______||_______|")
-    print("Welcome to 15 puzzle!!!")
-    print()
-
-def asciiInsertFileName():
-    print(" ___                  _     ___ _ _                         ")
-    print("|_ _|_ _  ___ ___ _ _| |_  | __(_) |___ _ _  __ _ _ __  ___ ")
-    print("|  || ' \(_-</ -_) '_|  _| | _|| | / -_) ' \/ _` | '  \/ -_)")
-    print("|___|_||_/__/\___|_|  \__| |_| |_|_\___|_||_\__,_|_|_|_\___|")
-
-def asciiSolvingPuzzle():
-    print(" ___      _     _                 ")
-    print("/ __| ___| |_ _(_)_ _  __ _       ")
-    print("\__ \/ _ \ \ V / | ' \/ _` |_ _ _ ")
-    print("|___/\___/_|\_/|_|_||_\__, (_|_|_)")
-    print("                    |___/       ")
-    print()
-
-def asciiFound():
-    print("___                 _ _ ")
-    print("| __|__ _  _ _ _  __| | |")
-    print("| _/ _ \ || | ' \/ _` |_|")
-    print("|_|\___/\_,_|_||_\__,_(_)")
-    print("Persoalan dapat diselesaikan")
-    print()
-
-def asciiNotFound():
-    print(" _  _     _     ___                 _ _ ")
-    print("| \| |___| |_  | __|__ _  _ _ _  __| | |")
-    print("| .` / _ \  _| | _/ _ \ || | ' \/ _` |_|")
-    print("|_|\_\___/\__| |_|\___/\_,_|_||_\__,_(_)")
-    print("Persoalan tidak bisa diselesaikan")
-    print()
-
-def asciiFailedToSolve():
-    print("   ___                  ")
-    print("  / _ \ _ __ _ __ ______")
-    print(" | (_) | '_ \ '_ (_-<_-<")
-    print("  \___/| .__/ .__/__/__/")
-    print("       |_|  |_|         ")
-    print("You've waited more than 30 minutes!")
-    print("Try again or Try another puzzle")
-    print()
